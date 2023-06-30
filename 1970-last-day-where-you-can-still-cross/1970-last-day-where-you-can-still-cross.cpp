a5acf861df0559569ef98b8c182dfea51b036e1d
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2023-06-30 23:10:42 +0530

    Time: 861 ms (19.81%), Space: 240.4 MB (13.06%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
-    bool isPossible(int m, int n, int t, vector<vector<int>>& cells) {
-        vector<vector<int>> grid(m + 1, vector<int>(n + 1, 0)); // Grid representation
-        vector<pair<int, int>> directions {{1, 0}, {-1, 0}, {0, 1}, {0, -1}}; // Possible directions
-
-        for (int i = 0; i < t; i++) {
-            grid[cells[i][0]][cells[i][1]] = 1; // Mark cells from the given list as blocked
-        }
-
-        queue<pair<int, int>> q;
-        
-        for (int i = 1; i <= n; i++) {
-            if (grid[1][i] == 0) {
-                q.push({1, i}); // Start BFS from the top row
-                grid[1][i] = 1; // Mark the cell as visited
+    int n,m;
+    int d[5] = {0,1,0,-1,0};
+    bool dfs(vector<vector<int>>&vis,vector<vector<int>>&v,int i,int j){
+        vis[i][j]=1;
+        if(i==n-1)return true;
+        bool ans = false;
+        for(int k=0;k<4;k++){
+            int ni = i + d[k];
+            int nj = j + d[k+1];
+            if(ni>=0 && nj>=0 && ni<n && nj<m && !vis[ni][nj] && !v[ni][nj]){
+                ans |= dfs(vis,v,ni,nj);
             }
         }
-        while (!q.empty()) {
-            pair<int, int> p = q.front();
-            q.pop();
-            int r = p.first, c = p.second; // Current cell coordinates
-            for (auto d : directions) {
-                int nr = r + d.first, nc = c + d.second; // Neighbor cell coordinates
-                if (nr > 0 && nc > 0 && nr <= m && nc <= n && grid[nr][nc] == 0) {
-                    grid[nr][nc] = 1; // Mark the neighbor cell as visited
-                    if (nr == m) {
-                        return true; // Found a path to the bottom row
-                    }
-                    q.push({nr, nc}); // Add the neighbor cell to the queue for further exploration
-                }
-            }
-        }
-        return false; // Unable to find a path to the bottom row
+        return ans;
     }
-
-    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        int left = 0, right = row * col, latestPossibleDay = 0;
-        while (left < right - 1) {
-            int mid = left + (right - left) / 2; // Calculate the mid-day
-            if (isPossible(row, col, mid, cells)) {
-                left = mid; // Update the left pointer to search in the upper half
-                latestPossibleDay = mid; // Update the latest possible day
-            } else {
-                right = mid; // Update the right pointer to search in the lower half
+    int latestDayToCross(int row, int col, vector<vector<int>>& cell) {
+        int l=0,r=cell.size()-1;
+        n = row;
+        m = col;
+        int day=0;
+        while(l<=r){
+            int mid = (l+r)/2;
+            vector<vector<int>>v(row,vector<int>(col,0));
+            for(int i=0;i<=mid;i++){
+                v[cell[i][0]-1][cell[i][1]-1]=1;
             }
+           vector<vector<int>>vis(row,vector<int>(col,0));
+           bool ans=false;
+           for(int i=0;i<col;i++){
+               if(vis[0][i]==0 && v[0][i]==0){
+                   ans |= dfs(vis,v,0,i);
+               }
+           }
+           if(ans){
+               day=mid;
+               l=mid+1;
+           }
+           else{
+               r = mid - 1;
+           }
         }
-        return latestPossibleDay;
+        return day+1;
     }
 };
