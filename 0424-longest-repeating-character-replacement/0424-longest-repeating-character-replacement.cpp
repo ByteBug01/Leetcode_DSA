ca4696030559fef2429351573ab38cf4a4467395
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2023-08-27 00:02:52 +0530

    Time: 0 ms (100.00%), Space: 7 MB (77.17%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+      int n = s.size();
+    int max_length = 0;  // To store the maximum length of substring with same characters
+    int max_count = 0;   // To store the maximum count of any character in the window
+    vector<int> char_count(26, 0);  // To store the count of characters in the window
+    
+    int left = 0;  // Left pointer of the sliding window
+    
+    for (int right = 0; right < n; ++right) {
+        char_count[s[right] - 'A']++;
+        max_count = max(max_count, char_count[s[right] - 'A']);
+        
+        // Calculate the number of characters to change to make all characters in the window the same
+        // This is given by the formula: window size - max count
+        while ((right - left + 1) - max_count > k) {
+            char_count[s[left] - 'A']--;
+            left++;
+        }
+        
+        // Update the max_length with the current window size
+        max_length = max(max_length, right - left + 1);
+    }
+    
+    return max_length;   
+    }
+};
