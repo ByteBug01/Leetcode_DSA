0477ddd35bc8a870c1a08ffd1c59b6144940669d
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2023-10-10 22:10:34 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (6.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
+    //selection sort // TC = n^2
     void sortColors(vector<int>& a) {
-         int mini, i, j;
-    for( i = 0; i<a.size()-1; i++){
-        mini = i;
-        for( j=i+1; j<a.size(); j++){
-            if(a[j]<a[mini]){
-                mini=j;
-            }
+//          int mini, i, j;
+//     for( i = 0; i<a.size()-1; i++){
+//         mini = i;
+//         for( j=i+1; j<a.size(); j++){
+//             if(a[j]<a[mini]){
+//                 mini=j;
+//             }
             
-        }
-        if(mini!=i)
-    swap(a[i], a[mini]);
-    }
-    }
+//         }
+//         if(mini!=i)
+//     swap(a[i], a[mini]);
+        
+        
+//     }
+        int n= a.size();
+        int key;
+        for(int i = 1; i<n; i++){
+            key = a[i];
+            int j = i-1;
+            while(j>=0 && a[j]>key){
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1]=key;
+        } // Insertion Sort TC=o(n^2)
+    } 
 };
