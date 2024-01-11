b64aaa1b6a5cd418127311d6327fe1e2ca4a5aaf
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-01-11 23:55:03 +0530

    Time: 3 ms (85.41%), Space: 9.9 MB (95.67%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -11,34 +11,51 @@
  */
 class Solution {
 public:
-    int maxdiff;
+//     int maxdiff;
     
-    void findMaxDiff1(TreeNode* root, TreeNode* child){
-        if(root == NULL || child == NULL) return;
+//     void findMaxDiff1(TreeNode* root, TreeNode* child){
+//         if(root == NULL || child == NULL) return;
         
-        maxdiff = max(maxdiff, abs(root->val - child->val));
+//         maxdiff = max(maxdiff, abs(root->val - child->val));
         
-        findMaxDiff1(root, child->left);
-        findMaxDiff1(root, child->right);
+//         findMaxDiff1(root, child->left);
+//         findMaxDiff1(root, child->right);
         
-    }
+//     }
     
-    void findMaxDiff2(TreeNode* root){
-        if(root == NULL) return ;
+//     void findMaxDiff2(TreeNode* root){
+//         if(root == NULL) return ;
         
-        findMaxDiff1(root, root->left);
-        findMaxDiff1(root, root->right);
+//         findMaxDiff1(root, root->left);
+//         findMaxDiff1(root, root->right);
         
-        findMaxDiff2( root->left);
-        findMaxDiff2( root->right);
+//         findMaxDiff2( root->left);
+//         findMaxDiff2( root->right);
+//     }
+    
+    //opimized---------------//
+    int findMaxDiff(TreeNode* root, int maxii, int minii){
+        if(root == NULL) {
+            return abs(maxii - minii);
+        }
+        
+        maxii = max(maxii, root->val );
+        minii = min(minii, root->val);
+        
+        int l =  findMaxDiff(root->left, maxii, minii); //path1
+        int r =  findMaxDiff(root->right, maxii, minii); // path2
+        
+        return max(l, r); // maximum value between paths
     }
     
     
     int maxAncestorDiff(TreeNode* root) {
-        maxdiff = -1;
+//         maxdiff = -1;
         
-        findMaxDiff2(root);
+//         findMaxDiff2(root);
         
-        return maxdiff;
+//         return maxdiff;
+        
+        return  findMaxDiff(root, root->val, root->val);
     }
 };
