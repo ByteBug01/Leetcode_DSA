d593a0407d28d1fe328c9ca0c376a37a64f1e5e6
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-01-20 00:24:59 +0530

    Time: 11 ms (80.66%), Space: 13.1 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -13,30 +13,133 @@ public:
         
 //         return dp[i][j] = min(st,min(ld,rd));
 //     }
-    int minFallingPathSum(vector<vector<int>>& A) {
-      int m = A.size();
+    
+    
+    
+    //....................
+    
+//     int minFallingPathSum(vector<vector<int>>& A) {
+//       int m = A.size();
 
-        vector<vector<int>>dp(m, vector<int>(m, 0));
+//         vector<vector<int>>dp(m, vector<int>(m, 0));
         
-        for (int i = 0; i < m; i++)
-            dp[0][i] = A[0][i];
+//         for (int i = 0; i < m; i++)
+//             dp[0][i] = A[0][i];
         
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                if (j == 0)
-                    dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j+1]);
-                else if (j == m-1)
-                    dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
-                else
-                    dp[i][j] = A[i][j] + min(dp[i-1][j], min(dp[i-1][j-1], dp[i-1][j+1]));
+//         for (int i = 1; i < m; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (j == 0)
+//                     dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j+1]);
+//                 else if (j == m-1)
+//                     dp[i][j] = A[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
+//                 else
+//                     dp[i][j] = A[i][j] + min(dp[i-1][j], min(dp[i-1][j-1], dp[i-1][j+1]));
+//             }
+//         }
+        
+//         int ans = INT_MAX;
+//         for (int j = 0; j < m; j++) {
+//             ans = min(ans, dp[m-1][j]);
+//         }
+        
+//         return ans;
+//     }
+    
+    //............
+//     int n ; 
+//     int t[101][101];
+    
+//     int solve(vector<vector<int>> &A, int row, int col, vector<vector<int>>& t){
+        
+//         if(row == n-1){
+//             return A[row][col];
+//         }
+        
+//         //..............
+//         if(t[row][col] != -1){
+//             return t[row][col];
+//         }
+//         //...............
+        
+//         int sum = A[row][col];
+        
+//         int minSum = INT_MAX;
+        
+//         if(row+1 < n && col>= 0)
+//        minSum = min(minSum, sum+solve(A, row+1, col-1,t )) ;
+      
+//         if(row+1<n)
+//         minSum = min(minSum, sum+solve(A, row+1, col,t)) ;
+       
+//         if(row+1< n && col+1 <n)
+//         minSum = min(minSum, sum+ solve(A, row+1, col+1, t));
+        
+//         return t[row][col] = minSum;
+//     }
+    
+    
+//      int minFallingPathSum(vector<vector<int>>& A) {
+     
+//          n = A.size();
+//           memset(t, -1, sizeof(t));
+         
+//          int row = 0;
+         
+//          int ans = INT_MAX;
+     
+//      for(int col = 0; col<n; col++){
+//          ans = min(ans, solve(A, row, col, t));
+//      }
+     
+     
+//      return ans;
+//      }
+    //...........
+//      int MFS(vector<vector<int>>& A, int row, int col, vector<vector<int>>& t) {
+//         if(row == A.size()-1)
+//             return A[row][col];
+//         if(t[row][col] != -1)
+//             return t[row][col];
+        
+//         int minSum = INT_MAX;
+        
+//         for(int shift = -1; shift<=1; shift++) {
+//             if(col + shift >=0 && col+shift < A[row].size()) {
+//                 minSum = min(minSum, A[row][col] + MFS(A, row+1, col+shift, t));
+//             }
+//         }
+        
+//         return t[row][col] = minSum;
+//     }
+//     int minFallingPathSum(vector<vector<int>>& A) {
+//         int m = A.size(); //row
+//         int n = m; //column
+//         vector<vector<int>> t(101, vector<int>(101));
+//         for(int i = 0; i<101; i++) {
+//             for(int j = 0; j<101; j++) {
+//                 t[i][j] = -1;
+//             }
+//         }
+//         int result = INT_MAX;
+//         for(int col = 0; col<n; col++) {
+//             result = min(result, MFS(A, 0, col, t));
+//         }
+//         return result;
+//     }
+    
+     int minFallingPathSum(vector<vector<int>>& A) {
+        int n = A.size();
+        vector<int> prev(n);
+        for(int col = 0; col<n; col++)
+            prev[col] = A[0][col];
+        
+        for(int row = 1; row<n; row++) {
+            vector<int> curr(n);
+            for(int col = 0; col<n; col++) {
+                curr[col] = A[row][col] + min({prev[max(0, col-1)],  prev[col],  prev[min(n-1, col+1)]});
             }
+            prev = curr;
         }
-        
-        int ans = INT_MAX;
-        for (int j = 0; j < m; j++) {
-            ans = min(ans, dp[m-1][j]);
-        }
-        
-        return ans;
+        return *min_element(prev.begin(), prev.end());
     }
 };
