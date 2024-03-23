c1306446f7720715a3ef032ab44eb826bbea2c1f
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-03-24 00:23:24 +0530

    Time: 13 ms (36.39%), Space: 22.3 MB (58.79%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,47 +3,75 @@ public:
     
     //normal array and logic based
     
-    vector<int> getLeftMax(vector<int> & height, int & n){
+//     vector<int> getLeftMax(vector<int> & height, int & n){
         
-        vector<int> left(n);
+//         vector<int> left(n);
         
-        left[0] = height[0];
+//         left[0] = height[0];
         
-        for(int i = 1; i<n; i++){
-            left[i] = max(left[i-1], height[i]); //don't mistake
-        }
+//         for(int i = 1; i<n; i++){
+//             left[i] = max(left[i-1], height[i]); //don't mistake
+//         }
         
-        return left;
-    }
+//         return left;
+//     }
     
-    vector<int> getRightMax(vector<int> & height, int & n){
+//     vector<int> getRightMax(vector<int> & height, int & n){
         
-        vector<int> right(n);
+//         vector<int> right(n);
         
-    right[n-1] = height[n-1];
+//     right[n-1] = height[n-1];
         
-        for(int i = n-2; i>=0; i--){
-            right[i] = max(right[i+1], height[i]); // don't mistake
-        }
+//         for(int i = n-2; i>=0; i--){
+//             right[i] = max(right[i+1], height[i]); // don't mistake
+//         }
         
-        return right;
-    }
+//         return right;
+//     }
     
-    int trap(vector<int>& height) {
-        int n = height.size();
+//     int trap(vector<int>& height) {
+//         int n = height.size();
         
-        vector<int> leftMax = getLeftMax(height, n);
-        vector<int> rightMax = getRightMax(height, n);
+//         vector<int> leftMax = getLeftMax(height, n);
+//         vector<int> rightMax = getRightMax(height, n);
         
-        int sum = 0 ;
+//         int sum = 0 ;
         
-        for(int i = 0; i<n; i++){
+//         for(int i = 0; i<n; i++){
             
-            int h = min(leftMax[i], rightMax[i]) - height[i];
+//             int h = min(leftMax[i], rightMax[i]) - height[i];
             
-            sum += h;
-        }
+//             sum += h;
+//         }
         
-        return sum;
-    }
+//         return sum;
+//     }
+    
+    
+    // 2 pointer approach
+     int trap(vector<int>& height){
+         int i = 0; int j =  height.size()-1;
+         
+         int ans = 0;
+         
+        int leftmax = 0; int rightmax = 0;
+         
+         while(i<=j){
+             leftmax = max(leftmax, height[i]);
+             rightmax = max(rightmax, height[j]);
+             
+             if(leftmax < rightmax){
+                 ans += (leftmax - height[i]);
+                 i++;
+             }
+             
+             else{
+                 ans += (rightmax - height[j]);
+                 j--;
+             }
+         }
+         
+         return ans;
+     }
+    
 };
