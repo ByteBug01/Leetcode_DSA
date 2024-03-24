f2bfc5d1dcaf3270b44e67e327ce922b9626e6a4
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-03-24 19:45:49 +0530

    Time: 22 ms (22.40%), Space: 12.7 MB (56.70%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        int diff = INT_MAX;
-        for(int i=0; i<nums.size()-2;i++){
-            int newTarget = target - nums[i];
-            int l= i+1, r = nums.size()-1;
-            while(l<r){
-                int sum = nums[l]+nums[r];
-                diff = abs(diff)>abs(sum-newTarget) ? sum-newTarget : diff;
-                if(sum<newTarget)
-                    l++;
-                else
-                    r--;
+        // sort(nums.begin(), nums.end());
+        // int diff = INT_MAX;
+        // for(int i=0; i<nums.size()-2;i++){
+        //     int newTarget = target - nums[i];
+        //     int l= i+1, r = nums.size()-1;
+        //     while(l<r){
+        //         int sum = nums[l]+nums[r];
+        //         diff = abs(diff)>abs(sum-newTarget) ? sum-newTarget : diff;
+        //         if(sum<newTarget)
+        //             l++;
+        //         else
+        //             r--;
+        //     }
+        // }
+        // return target + diff;
+        
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        
+        int closestSum = 100000;
+        
+        for(int k = 0; k<=n-3; k++){
+            
+            int i  = k+1; int j = n-1;
+            
+            while(i<j){
+               int sum  = nums[k] + nums[i] + nums[j];
+                
+                if(abs(target - sum) < abs(target - closestSum)){
+                    closestSum = sum;
+                }
+                
+                if(sum < target) { i++; }
+                else { j--; }
+                
             }
         }
-        return target + diff;
+        
+            return closestSum;
     }
 };
