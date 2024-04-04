6abdb245f017e158e3c6ecf76f69f1e66e711469
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-04-04 12:43:59 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (22.85%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,24 +3,51 @@ public:
     //"(1+(2*3)+((8)/4))+1"
   // 0 1  2   1 23 2  1 0
     
+    //already given string as VPS
+    
+    //TC : O(n)
+    //SC : O(1)
+    
+//     int maxDepth(string s) {
+        
+//         int n  = s.length();
+        
+//        int  d = 0;
+//         int cnt = 0;
+//         for(int i = 0; i<n; i++){
+//             if(s[i] == '(') {
+//                 cnt++;
+//                 d = max(d, cnt);
+//             } 
+//             if(s[i] == ')'){
+//                 cnt--;
+//                 d = max(d, cnt);
+//             }
+            
+//         }
+        
+//         return d;
+//     }
+    
+    //--------------------------------//
+    // TC = O(n)
+    //SC = O(n)
+    
     int maxDepth(string s) {
+        int ans = 0 ;
+        stack<char> st;
         
-        int n  = s.length();
-        
-       int  d = 0;
-        int cnt = 0;
-        for(int i = 0; i<n; i++){
-            if(s[i] == '(') {
-                cnt++;
-                d = max(d, cnt);
-            } 
-            if(s[i] == ')'){
-                cnt--;
-                d = max(d, cnt);
+        for(char ch : s){
+            if(ch == '('){
+                st.push(ch);
+            }else if(ch == ')'){
+                st.pop();
             }
             
+            ans = max(ans, (int)st.size());
         }
         
-        return d;
+        
+        return ans;
     }
 };
