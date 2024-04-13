110674bc83e409e8126edeb3e6237fcedc635777
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-04-14 01:22:33 +0530

    Time: 29 ms (76.10%), Space: 17.4 MB (75.47%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    //solve again//
+     int maxRectangleInHistogram(vector<int> &v){
+        stack<int> st;
+        int maxA = 0;
+        int n = v.size();
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i==n || v[st.top()] >= v[i])){
+                int height = v[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i - st.top() -1;
+                maxA = max(maxA,width*height);
+
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& matrix) {
+         int m = matrix.size();
+        int n = matrix[0].size();
+        int res = 0;
+        vector<int> v(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == '1'){
+                    v[j]++;
+                }else{
+                    v[j] = 0;
+                }
+            }
+            int r = maxRectangleInHistogram(v);
+            res = max(res,r);
+        }
+        return res;
+    }
+};
