8df07e5fdb340f9b11c4878b65f146457d3f1936
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-04-27 01:42:25 +0530

    Time: 138 ms (62.26%), Space: 18.2 MB (57.39%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,35 +1,72 @@
+// class Solution {
+// public:
+    
+//     //recursion+memo
+//     int n;
+//     int t[201][201];
+//     int solve(int row, int col, vector<vector<int>>& grid) {
+//         if (row == n - 1) {
+//             return grid[row][col];
+//         }
+
+//         if(t[row][col] != -1) {
+//             return t[row][col];
+//         }
+        
+//         int ans = INT_MAX;
+//         for (int nextCol = 0; nextCol < n; nextCol++) {
+//             if (nextCol != col) {
+//                 ans = min(ans, solve(row + 1, nextCol, grid));
+//             }
+//         }
+
+//         return t[row][col] = grid[row][col] + ans;
+//     }
+    
+//     int minFallingPathSum(vector<vector<int>>& grid) {
+//         n = grid.size();
+//         memset(t, -1, sizeof(t));
+
+//         int result = INT_MAX;
+//         for (int col = 0; col < n; col++) {
+//             result = min(result, solve(0, col, grid));
+//         }
+
+//         return result;
+//     }
+// };
+
+//once again look
+//bottom - up
+
 class Solution {
 public:
-    
-    //recursion+memo
     int n;
-    int t[201][201];
-    int solve(int row, int col, vector<vector<int>>& grid) {
-        if (row == n - 1) {
-            return grid[row][col];
+    
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        vector<vector<int>> t(n, vector<int>(n, INT_MAX));
+
+        for (int col = 0; col < n; col++) {
+            t[n - 1][col] = grid[n - 1][col];
         }
 
-        if(t[row][col] != -1) {
-            return t[row][col];
-        }
-        
-        int ans = INT_MAX;
-        for (int nextCol = 0; nextCol < n; nextCol++) {
-            if (nextCol != col) {
-                ans = min(ans, solve(row + 1, nextCol, grid));
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col < n; col++) {
+                int ans = INT_MAX;
+                for (int nextCol = 0; nextCol < n; nextCol++) {
+                    if (nextCol != col) {
+                        ans = min(ans, t[row + 1][nextCol]);
+                    }
+                }
+                t[row][col] = grid[row][col] + ans;
             }
         }
 
-        return t[row][col] = grid[row][col] + ans;
-    }
-    
-    int minFallingPathSum(vector<vector<int>>& grid) {
-        n = grid.size();
-        memset(t, -1, sizeof(t));
-
         int result = INT_MAX;
         for (int col = 0; col < n; col++) {
-            result = min(result, solve(0, col, grid));
+            result = min(result, t[0][col]);
         }
 
         return result;
