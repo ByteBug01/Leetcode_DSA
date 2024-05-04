d76f521754285383b371337a0f1d6f802067ebd6
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-04 22:39:33 +0530

    Time: 83 ms (31.28%), Space: 70.3 MB (10.11%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,36 @@
+typedef long long ll;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+//         int sum =0;
+//         int maxi = nums[0];
+
+//         for(int i=0; i<nums.size(); i++){
+//             sum= sum+ nums[i];
+//             maxi = max(maxi, sum);
+
+//             if(sum<0)
+//             sum = 0;
+//         }
+//         return maxi;
+        
+        //kadan's algo
+        
+        ll sum = 0; ll maxi = LONG_MIN;
+        
+        for(int i = 0; i<nums.size(); i++){
+            sum += nums[i];
+            
+            if(sum > maxi){
+                maxi = sum;
+            }
+            
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        
+        return maxi;
+    }
+};
