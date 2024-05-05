c79aa0177dcb4429e7523dfa2c9fd7a5a521ed39
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-05 12:43:53 +0530

    Time: 5 ms (17.44%), Space: 9.8 MB (32.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,25 +3,49 @@ public:
     //Dutch National Flag
     void sortColors(vector<int>& arr)
    {
-        int n = arr.size();
-int low = 0, mid = 0, high = n - 1; // 3 pointers
+//         int n = arr.size();
+// int low = 0, mid = 0, high = n - 1; // 3 pointers
 
-    while (mid <= high) 
-    {
-        if (arr[mid] == 0) 
-        {
-            swap(arr[low], arr[mid]);
-            low++;
-            mid++;
+//     while (mid <= high) 
+//     {
+//         if (arr[mid] == 0) 
+//         {
+//             swap(arr[low], arr[mid]);
+//             low++;
+//             mid++;
+//         }
+//         else if (arr[mid] == 1) 
+//         {
+//             mid++;
+//         }
+//         else {
+//             swap(arr[mid], arr[high]);
+//             high--;
+//              }
+
+
+
+//swap method:
+
+int i = 0; //for 0
+int j  = 0; // for 1
+int k  =  arr.size() - 1; // for 2
+    
+
+    while(j<= k){
+        if(arr[j] == 1){
+            j++;
+        }else if(arr[j] == 2){
+            swap(arr[j], arr[k]);
+            k--;
         }
-        else if (arr[mid] == 1) 
-        {
-            mid++;
+        else{//arr[j] == 0
+        swap(arr[j] , arr[i]);
+        i++;
+        j++;
         }
-        else {
-            swap(arr[mid], arr[high]);
-            high--;
-             }
-    } 
+    }
+
+    
   }
 };
