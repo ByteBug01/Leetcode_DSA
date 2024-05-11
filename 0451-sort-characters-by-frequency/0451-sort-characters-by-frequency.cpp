8f47d7503627bd8c493d786cd0c316ffaf9cb98d
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-11 19:37:31 +0530

    Time: 8 ms (78.01%), Space: 11 MB (25.27%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,6 +1,6 @@
-class Solution {
-public:
-    string frequencySort(string s) {
+// class Solution {
+// public:
+//     string frequencySort(string s) {
 //         unordered_map<char,int>mp;
 //         for(int i=0;i<s.length();i++){
 //             mp[s[i]]++;
@@ -25,33 +25,70 @@ public:
 //         }
 //         return s;
         
-        int n = s.length();
+        //-------------------------------------------------
+    //     int n = s.length();
+        
+    //     unordered_map<char, int> mp;
+        
+    //     for(int i = 0; i<n; i++){
+    //         mp[s[i]]++;
+    //     }
+        
+    //     string ans = "";
+        
+    //     vector<std::pair<char, int>> vec(mp.begin(), mp.end());
+
+
+    //    sort(vec.begin(), vec.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
+    //     return a.second > b.second; 
+    // });
+
+        
+    //     for(auto it: vec){
+    //         int i = it.second ;
+    //         while(i--){
+    //             ans += it.first;
+    //         }
+    //     }
+        
+    //     return ans;
+        
+    //}
+
+// };
+
+//-------------------------------------------------------------
+//using prioity queue  O(nlong)
+
+class Solution {
+public:
+    typedef pair<char, int> P;
+    struct comp {
+        bool operator()(P &p1, P &p2) {
+            return p1.second<p2.second; //max-heap
+        }
+    };
+    
+    string frequencySort(string s) {
+        priority_queue<P, vector<P>, comp> pq;
         
         unordered_map<char, int> mp;
-        
-        for(int i = 0; i<n; i++){
-            mp[s[i]]++;
+        for(char &ch : s) {
+            mp[ch]++;
         }
         
-        string ans = "";
-        
-        vector<std::pair<char, int>> vec(mp.begin(), mp.end());
-
-
-       sort(vec.begin(), vec.end(), [](const pair<char, int>& a, const pair<char, int>& b) {
-        return a.second > b.second; 
-    });
-
-        
-        for(auto it: vec){
-            int i = it.second ;
-            while(i--){
-                ans += it.first;
-            }
+        for(auto &it : mp) {
+            pq.push({it.first, it.second});
         }
         
-        return ans;
+        string result = "";
         
+        while(!pq.empty()) {
+            P temp = pq.top();
+            pq.pop();
+            
+            result += string(temp.second, temp.first);
+        }
+        return result;
     }
-
 };
