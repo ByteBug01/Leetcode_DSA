c1f043a4da2d162b5146fe3d4595d9f1f7d02d8b
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-21 00:02:54 +0530

    Time: 15 ms (71.06%), Space: 18.9 MB (17.07%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,48 +1,103 @@
 
+// class Solution {
+// public:
+
+// // kahn's algo , toposort using bfs
+
+//    bool topoSortCheck(unordered_map<int, vector<int>> adj, int numCourses,  vector<int> & indegree){
+//  int n = numCourses;
+//         queue<int> q;
+//         int cnt = 0;
+
+//         for(int i = 0; i<n; i++){
+//                 if (indegree[i] == 0){
+//                     q.push(i);
+//                     cnt++;
+//                  }
+//         }
+
+// while(!q.empty()){
+//     int u = q.front();
+//     q.pop();
+
+//     for(auto & v: adj[u]){
+
+//         indegree[v]--;
+
+//         if(indegree[v] == 0){
+//             q.push(v);
+
+//             cnt++;
+//         }
+//     }
+// }
+
+// if(cnt == n){ // if cnt equal to no. of nodes then no cycle
+//     return true;
+// }
+
+// return false;
+//    }
+    
+  
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+//         unordered_map<int, vector<int>> adj;
+//         vector<int> indegree(numCourses, false);
+        
+//         for(auto& num : prerequisites){
+//             int a = num[0];
+//             int b = num[1];
+            
+//             adj[b].push_back(a);
+//             indegree[a]++;
+//         }
+        
+//         return topoSortCheck(adj, numCourses, indegree);
+//     }
+// };
+
+//-----------------------------------------------------------------------------
+//using dfs, topocheck
 class Solution {
 public:
-    
-    bool DFSRec(int s, vector<bool>&visited, vector<bool>&currVisited, vector<int>adj[]){
-        visited[s] = true;
-        currVisited[s] = true;
+
+bool isCycleDFS(unordered_map<int, vector<int>> &adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
         
-        vector<int>data = adj[s];
-        for(auto x: data){
-            if(!visited[x]){
-                if(DFSRec(x, visited, currVisited, adj)){
-                    return true;
-                }
-            }
-            else if(visited[x] && currVisited[x]){
+        
+        for(int &v : adj[u]) {
+            //if not visited, then we check for cycle in DFS
+            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
                 return true;
-            }
+            else if(inRecursion[v] == true)
+                return true;
         }
         
-        currVisited[s] = false;
+        inRecursion[u] = false;
         return false;
+        
     }
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+
+ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+             unordered_map<int, vector<int>> adj;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
         
-        int n = numCourses;
-        vector<bool>visited(n, false), currVisited(n, false);
-        
-        vector<int>adj[n];
-        for(auto x: prerequisites){
-            vector<int>data = x;
-            int a = data[0];
-            int b = data[1];
-            adj[a].push_back(b);
+        for(auto &vec : prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+            
+            //b ---> a
+            adj[b].push_back(a);
         }
         
-        for(int i=0; i<n; i++){
-            if(!visited[i]){
-                if(DFSRec(i, visited, currVisited, adj)){
-                    return false;
-                }
-            }
+        for(int i = 0; i<numCourses; i++) {
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion))
+                return false;
         }
         
         return true;
-    }
+ }
 };
