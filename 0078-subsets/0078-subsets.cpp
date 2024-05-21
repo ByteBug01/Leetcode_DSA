4b1a6e7b7aa5a27f72d3939c08e90d2248d1b22f
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-21 14:11:12 +0530

    Time: 4 ms (39.89%), Space: 14.9 MB (16.02%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -29,31 +29,64 @@
 //-------------------------------------------------
 //Recursion
 
+// class Solution {
+// public:
+
+// vector<vector<int>> ans;
+// int n;
+// void solve(vector<int>& nums,int i, vector<int> &tmp){
+//     //base case
+// if( i>= n){
+//     ans.push_back(tmp);
+//     return;
+// }
+
+// tmp.push_back(nums[i]); //take curr element
+// solve(nums, i+1, tmp);
+// tmp.pop_back(); //skip curr element
+// solve(nums, i+1, tmp);
+
+// }
+//  vector<vector<int>> subsets(vector<int>& nums) {
+//     n = nums.size();
+
+//             vector<int> tmp;
+
+//             solve(nums, 0, tmp);
+
+//             return ans;
+//  }
+// };
+
+
 class Solution {
 public:
 
-vector<vector<int>> ans;
-int n;
-void solve(vector<int>& nums,int i, vector<int> &tmp){
-    //base case
-if( i>= n){
-    ans.push_back(tmp);
-    return;
-}
+//recursion-----------------------------
+ void solve(vector<int> nums, vector<int> output, int idx, vector<vector<int> >& ans) {
+        //base case
+        if(idx >= nums.size()) {
+            ans.push_back(output);
+            return ;
+        }
+        
+        //exclude
+        solve(nums, output, idx+1, ans);
+        
+        //include
+        int element = nums[idx];
+        output.push_back(element);
+        solve(nums, output, idx+1, ans);
+    }
 
-tmp.push_back(nums[i]); //take curr element
-solve(nums, i+1, tmp);
-tmp.pop_back(); //skip curr element
-solve(nums, i+1, tmp);
-
-}
- vector<vector<int>> subsets(vector<int>& nums) {
-    n = nums.size();
-
-            vector<int> tmp;
-
-            solve(nums, 0, tmp);
-
-            return ans;
- }
+  vector<vector<int>> subsets(vector<int>& nums) {
+        //  int n  = nums.size();
+          vector<vector<int> > ans;
+        vector<int> output;
+        int idx = 0;
+        solve(nums, output, idx, ans);
+        return ans;
+        
+  }
 };
+
