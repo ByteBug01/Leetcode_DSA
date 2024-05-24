0a17b431059f3f4d2fe84305fc6843a3a334a232
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-05-25 02:19:05 +0530

    Time: 17 ms (48.37%), Space: 20 MB (80.65%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -10,28 +10,52 @@
  */
 
  //approach -1 by rand() function
-class Solution {
-public:
-vector<int> arr;
-    Solution(ListNode* head) {
-        ListNode* temp = head;
-        while(temp != NULL){
-arr.push_back(temp->val);
-temp =  temp ->next;
-        }
-    }
+// class Solution {
+// public:
+// vector<int> arr;
+//     Solution(ListNode* head) {
+//         ListNode* temp = head;
+//         while(temp != NULL){
+// arr.push_back(temp->val);
+// temp =  temp ->next;
+//         }
+//     }
     
-    int getRandom() {
-        int n = arr.size();
+//     int getRandom() {
+//         int n = arr.size();
 
-        int random_idx = rand()% n;
+//         int random_idx = rand()% n;
 
-        return arr[random_idx];
-    }
-};
+//         return arr[random_idx];
+//     }
+// };
 
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution* obj = new Solution(head);
  * int param_1 = obj->getRandom();
  */
+
+//appproach - 2--------------------------Sampling Reservoir(google)----------------------------
+class Solution {
+public:
+    ListNode* temp;
+    Solution(ListNode* head) {
+        temp = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int res;
+        ListNode* curr = temp;
+        int size = 1;
+        while(curr) {
+            //choosing probability is 1/size (reservior sampling)
+            if(rand()%size == 0)
+                res = curr->val;
+            curr = curr->next;
+            size++;
+        }
+        return res;
+    }
+};
