9bee857aa398c65f5048a01045f909f4662cbad3
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-02 18:07:46 +0530

    Time: 11 ms (82.18%), Space: 19.2 MB (57.91%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,18 +2,28 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
 
-        unordered_map<int, int> mp;
+        // unordered_map<int, int> mp;
 
-        for(auto num: nums){
-           mp[num]++;
-        }
+        // for(auto num: nums){
+        //    mp[num]++;
+        // }
         
-        int ans;
-        for(auto & it: mp){
-            if(it.second == 1){
-                 ans = it.first;
-            }
+        // int ans;
+        // for(auto & it: mp){
+        //     if(it.second == 1){
+        //          ans = it.first;
+        //     }
+        // }
+        // return ans;
+
+        int ans = 0;
+        for(auto & num: nums){
+            ans^=num;
         }
         return ans;
     }
 };
+// USING BITWISE XOR OPERATOR (USING CONSTANT SPACE )
+//A^A=0
+//A^B^A=B
+//here we, don't need to use unordered_map and sorting
