40e4cf235c6373d363c0c0ec1c87e4e9f1236a03
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-05 23:17:00 +0530

    Time: 0 ms (100%), Space: 13.4 MB (66.1%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,25 +11,28 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* list1 = new ListNode(0);
-        ListNode* list2 = new ListNode(0);
-        
-        ListNode* l1 = list1;
-        ListNode* l2 = list2;
-        while(head){
-            if(head->val<x)
-            {
-                l1->next = head;
-                l1 = l1->next;
-            }
-            else{
-                l2->next=head;
-l2=l2->next;
-            }
-            head  = head->next;
+       ListNode* small = new ListNode(0);
+       ListNode* large = new ListNode(0); // 0 is the dummy value
+
+       ListNode* smallp = small; // smallpointer
+       ListNode* largep = large;
+
+       while(head != NULL){
+        if(head->val < x){
+            smallp->next = head;
+            smallp = smallp->next;
+        }else{
+            largep->next = head;
+            largep = largep->next;
         }
-        l1->next = list2->next;
-        l2->next = NULL;
-        return list1->next;
+
+        head = head->next;
+       }
+
+       smallp->next = large->next; //smallpointer ka next large ka next hoga
+       largep->next = NULL;
+
+       return small->next;
+
     }
 };
