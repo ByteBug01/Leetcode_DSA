559fad24d74ce849b4cd08d1599ccb8529bd1ef0
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-07 01:00:56 +0530

    Time: 51 ms (85.74%), Space: 16.8 MB (77.47%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,10 +1,76 @@
+// class Solution {
+// public:
+// int n;
+
+// //memoization //LIS VARIENT -------------------------------------------------
+
+// int t[1001][1001];
+
+// bool isPredecessor(string &s1, string& s2){
+//     int m = s1.length();
+//     int n =s2.length();
+//     if(m >= n || n-m != 1){
+// return false;
+//     }
+//     int i = 0;
+//     int j = 0;
+
+//     while(i < m && j < n){
+//         if(s1[i] == s2[j]){
+//             i++;
+//         }
+//         j++;
+//     }
+
+//     return i == m;
+// }
+
+// static bool comp(string& s1, string &s2){
+//     return s1.length() < s2.length(); //jiska length chotta hai wo phle aaye
+// }
+
+// int LIS(vector<string>& words, int p, int curr_idx){
+
+//     if(curr_idx == n) return 0;
+
+//     if(p != -1 && t[p][curr_idx] != -1){
+//         return t[p][curr_idx];
+//     }
+
+//     int take = 0, not_take = 0;
+
+//     if(p == -1 || isPredecessor(words[p], words[curr_idx])){
+//         take = 1 + LIS(words, curr_idx, curr_idx+1);
+//     }
+
+//     not_take = LIS(words, p, curr_idx+1);
+
+//     if(p != -1){
+//         t[p][curr_idx] = max(take, not_take);
+//     }
+
+//     return max(take, not_take);
+// }
+
+
+//     int longestStrChain(vector<string>& words) {
+//          n = words.size();
+
+//          sort(words.begin(), words.end(), comp);
+
+//          memset(t, -1, sizeof(t));
+//          int prev_idx = -1;
+//          int curr_idx = 0;
+
+//          return LIS(words, prev_idx, curr_idx);
+//     }
+// };
+
+// BOTTOM-UP ---------------------------------------------------------------------
+
 class Solution {
 public:
 int n;
-
-//memoization //LIS VARIENT -------------------------------------------------
-
-int t[1001][1001];
 
 bool isPredecessor(string &s1, string& s2){
     int m = s1.length();
@@ -25,43 +91,32 @@ return false;
     return i == m;
 }
 
+//comparator function for sorting according to it---------
+
 static bool comp(string& s1, string &s2){
     return s1.length() < s2.length(); //jiska length chotta hai wo phle aaye
 }
 
-int LIS(vector<string>& words, int p, int curr_idx){
-
-    if(curr_idx == n) return 0;
-
-    if(p != -1 && t[p][curr_idx] != -1){
-        return t[p][curr_idx];
-    }
-
-    int take = 0, not_take = 0;
-
-    if(p == -1 || isPredecessor(words[p], words[curr_idx])){
-        take = 1 + LIS(words, curr_idx, curr_idx+1);
-    }
-
-    not_take = LIS(words, p, curr_idx+1);
-
-    if(p != -1){
-        t[p][curr_idx] = max(take, not_take);
-    }
-
-    return max(take, not_take);
-}
-
-
-    int longestStrChain(vector<string>& words) {
+ int longestStrChain(vector<string>& words) {
          n = words.size();
 
          sort(words.begin(), words.end(), comp);
 
-         memset(t, -1, sizeof(t));
-         int prev_idx = -1;
-         int curr_idx = 0;
+         int ans = 1;
 
-         return LIS(words, prev_idx, curr_idx);
-    }
+         vector<int> t(n, 1);
+
+         for(int i = 0; i<n; i++){
+            for(int j = 0; j<i; j++){
+
+                if(isPredecessor(words[j], words[i])){
+                  t[i] = max(t[i], t[j]+1);
+                  ans = max(ans, t[i]);
+                }
+            }
+         }   
+
+         return ans;   
+
+ }
 };
