13afd06606712b3c33515043cdf0dc0ddd0d4991
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-12 14:51:57 +0530

    Time: 0 ms (100%), Space: 10 MB (5.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,51 +1,75 @@
+// class Solution {
+// public:
+//     //Dutch National Flag
+//     void sortColors(vector<int>& arr)
+//    {
+// //         int n = arr.size();
+// // int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+// //     while (mid <= high) 
+// //     {
+// //         if (arr[mid] == 0) 
+// //         {
+// //             swap(arr[low], arr[mid]);
+// //             low++;
+// //             mid++;
+// //         }
+// //         else if (arr[mid] == 1) 
+// //         {
+// //             mid++;
+// //         }
+// //         else {
+// //             swap(arr[mid], arr[high]);
+// //             high--;
+// //              }
+
+
+
+// //swap method:
+
+// int i = 0; //for 0
+// int j  = 0; // for 1
+// int k  =  arr.size() - 1; // for 2
+    
+
+//     while(j<= k){
+//         if(arr[j] == 1){
+//             j++;
+//         }else if(arr[j] == 2){
+//             swap(arr[j], arr[k]);
+//             k--;
+//         }
+//         else{//arr[j] == 0
+//         swap(arr[j] , arr[i]);
+//         i++;
+//         j++;
+//         }
+//     }
+//   }
+// };
+
 class Solution {
 public:
     //Dutch National Flag
-    void sortColors(vector<int>& arr)
-   {
-//         int n = arr.size();
-// int low = 0, mid = 0, high = n - 1; // 3 pointers
+    void sortColors(vector<int>& nums){
+        int n = nums.size();
+ 
+        int   i = 0; //denotes for 0
+        int j  =0;   // for 1
+         int k =   n-1; // for 2
 
-//     while (mid <= high) 
-//     {
-//         if (arr[mid] == 0) 
-//         {
-//             swap(arr[low], arr[mid]);
-//             low++;
-//             mid++;
-//         }
-//         else if (arr[mid] == 1) 
-//         {
-//             mid++;
-//         }
-//         else {
-//             swap(arr[mid], arr[high]);
-//             high--;
-//              }
-
-
-
-//swap method:
-
-int i = 0; //for 0
-int j  = 0; // for 1
-int k  =  arr.size() - 1; // for 2
-    
-
-    while(j<= k){
-        if(arr[j] == 1){
-            j++;
-        }else if(arr[j] == 2){
-            swap(arr[j], arr[k]);
-            k--;
-        }
-        else{//arr[j] == 0
-        swap(arr[j] , arr[i]);
-        i++;
-        j++;
-        }
-    }
-
-    
-  }
+         while(j<=k){
+            if(nums[j] == 1){
+                j++;
+            }else if(nums[j] == 2){
+                swap(nums[j], nums[k]);
+                k--;
+            }else{
+                //nums[j] == 0
+                swap(nums[j], nums[i]);
+                i++;
+                j++;
+            }
+         }
+   }
 };
