26ac4e2eaefed57e93978e3800a6404786840394
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-19 00:28:45 +0530

    Time: 91 ms (24.1%), Space: 41.3 MB (39.43%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,36 +1,66 @@
+// class Solution {
+// public:
+//     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+//          int n = profit.size();
+//         vector<pair<int, int>> pd(n);
+
+//         for(int i = 0; i < n; i++) {
+//             pd[i] = {difficulty[i], profit[i]};
+//         }
+
+//         sort(pd.begin(), pd.end());
+
+//         vector<int> maxProfit(n);
+//         maxProfit[0] = pd[0].second;
+
+//         for(int i = 1; i < n; i++) {
+//             maxProfit[i] = max(maxProfit[i-1], pd[i].second);
+//         }
+
+//         sort(worker.begin(), worker.end());
+
+//         int ans = 0;
+//         int j = 0;
+
+//         for(int i = 0; i < worker.size(); i++) {
+//             while(j < n && worker[i] >= pd[j].first) {
+//                 j++;
+//             }
+//             if(j > 0) {
+//                 ans += maxProfit[j-1];
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-         int n = profit.size();
-        vector<pair<int, int>> pd(n);
+      int n = profit.size();
+      int m  =worker.size();
 
-        for(int i = 0; i < n; i++) {
-            pd[i] = {difficulty[i], profit[i]};
+      priority_queue<pair<int, int>> pd;
+
+      for(int i =0; i<n; i++){
+        pd.push({ profit[i], difficulty[i]});
+      }
+
+      sort(worker.begin(), worker.end(), greater<int>()); //desc order
+
+      int ans = 0;
+      int i = 0;
+
+      while(i < m && !pd.empty()){
+        if(pd.top().second > worker[i]){
+            pd.pop();
+        }else{
+            ans += pd.top().first;
+            i++;
         }
+      }
+return ans;
 
-        sort(pd.begin(), pd.end());
-
-        vector<int> maxProfit(n);
-        maxProfit[0] = pd[0].second;
-
-        for(int i = 1; i < n; i++) {
-            maxProfit[i] = max(maxProfit[i-1], pd[i].second);
-        }
-
-        sort(worker.begin(), worker.end());
-
-        int ans = 0;
-        int j = 0;
-
-        for(int i = 0; i < worker.size(); i++) {
-            while(j < n && worker[i] >= pd[j].first) {
-                j++;
-            }
-            if(j > 0) {
-                ans += maxProfit[j-1];
-            }
-        }
-
-        return ans;
     }
 };
