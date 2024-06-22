14a3883d9df25ab4290aff3aa3f052fbee6f61f3
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-22 19:18:21 +0530

    Time: 3 ms (59.83%), Space: 13.7 MB (6.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,61 +1,112 @@
-class Solution {
-public:
+// class Solution {
+// public:
     
     
-    int search(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         int left= 0;
-//         int right = n-1;
-//         int mid;
+//     int search(vector<int>& nums, int k) {
+// //         int n = nums.size();
+// //         int left= 0;
+// //         int right = n-1;
+// //         int mid;
         
-//         while(left<=right){
-//             mid = (left+right)/2;
-//             if(nums[mid]==target)
-//                 return mid;
-//             else if(nums[mid]>=nums[left]){
-//                 if(target<=nums[mid] && target>=nums[left])
-//                     right = mid-1;
-//                 else
-//                     left = mid+1;
+// //         while(left<=right){
+// //             mid = (left+right)/2;
+// //             if(nums[mid]==target)
+// //                 return mid;
+// //             else if(nums[mid]>=nums[left]){
+// //                 if(target<=nums[mid] && target>=nums[left])
+// //                     right = mid-1;
+// //                 else
+// //                     left = mid+1;
+// //             }
+// //             else
+// //             {
+// //                 if(target>=nums[mid] && target<=nums[right])
+// //                     left = mid+1;
+// //                 else
+// //                     right = mid-1;
+// //             }
+// //         }
+// //         return -1;
+        
+//         int n = nums.size();
+//         int low =0, high = n-1;
+//         while(low<=high)
+//         {
+//           int  mid=(low+high)/2;
+//             if(nums[mid]==k) return mid;
+//             else if(nums[low]<= nums[mid])
+//             {
+//                  if(nums[low]<= k && nums[mid]>=k)
+//                  {
+//                      high = mid-1;
+//                  }else
+//                  {
+//                      low=mid+1;
+//                  }
 //             }
+            
 //             else
 //             {
-//                 if(target>=nums[mid] && target<=nums[right])
-//                     left = mid+1;
-//                 else
-//                     right = mid-1;
+//                 if(nums[mid]<= k && nums[high]>= k)
+//                 {
+//                     low = mid+1;
+//                 }else
+//                 {
+//                     high=mid-1;
+//                 }
 //             }
 //         }
 //         return -1;
-        
-        int n = nums.size();
-        int low =0, high = n-1;
-        while(low<=high)
-        {
-          int  mid=(low+high)/2;
-            if(nums[mid]==k) return mid;
-            else if(nums[low]<= nums[mid])
-            {
-                 if(nums[low]<= k && nums[mid]>=k)
-                 {
-                     high = mid-1;
-                 }else
-                 {
-                     low=mid+1;
-                 }
-            }
-            
-            else
-            {
-                if(nums[mid]<= k && nums[high]>= k)
-                {
-                    low = mid+1;
-                }else
-                {
-                    high=mid-1;
-                }
-            }
+//     }
+// };
+
+class Solution {
+public:
+
+int find_pivot(vector<int>& nums, int n){
+    int l = 0; int r = n-1;
+
+    while(l < r){
+        int mid = l + (r - l)/2;
+
+        if(nums[mid] > nums[r]){
+            l = mid+1;
+        }else{
+            r = mid;
         }
-        return -1;
+    }
+    return r;
+}
+
+int binary_search(int l, int r, vector<int> & nums, int k){
+  int idx  =-1;
+    while(l <= r){
+        int mid = l + (r- l)/2;
+        
+        if(nums[mid] == k) 
+        {idx = mid; break;}
+       else if(nums[mid] < k){
+        l = mid+1;
+       }else{
+        r = mid -1;
+       }
+    }
+return idx;
+}
+
+    int search(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int pivot_idx = find_pivot(nums, n);
+
+        int idx = binary_search(0, pivot_idx-1, nums, k);
+
+        if(idx != -1){
+            return idx;
+        }
+
+        idx = binary_search(pivot_idx, n-1, nums, k);
+
+        return idx;
     }
 };
