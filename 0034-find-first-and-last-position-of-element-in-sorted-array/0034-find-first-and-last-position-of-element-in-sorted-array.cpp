f08a81c7dcf5e371d2b86d39b1a40a4b2451922d
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-22 20:40:15 +0530

    Time: 3 ms (89.16%), Space: 16.1 MB (49.56%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,99 @@
-class Solution {
-public:
-    vector<int> searchRange(vector<int>& a, int t) {
-//      bool flag=false;
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& a, int t) {
+// //      bool flag=false;
 
-//       vector<int> ans;
-//       long long n= a.size();
-//         for(int i=0; i<n; i++){
-//             if(a[i]==t)
-//             ans.push_back(i);
-//             flag = true;
-//             break;
-//         }
-//         for(int j=n-1; j>=0; j--){
-//             if(a[j]==t)
-//             ans.push_back(j);
-//             flag = true;
-//             break;
-//         }
-//         if(flag)
-//         return ans;
-//          else
-//         return {-1,-1};
+// //       vector<int> ans;
+// //       long long n= a.size();
+// //         for(int i=0; i<n; i++){
+// //             if(a[i]==t)
+// //             ans.push_back(i);
+// //             flag = true;
+// //             break;
+// //         }
+// //         for(int j=n-1; j>=0; j--){
+// //             if(a[j]==t)
+// //             ans.push_back(j);
+// //             flag = true;
+// //             break;
+// //         }
+// //         if(flag)
+// //         return ans;
+// //          else
+// //         return {-1,-1};
         
-          vector<int> ans = {-1, -1}; // Initialize the result vector with default values.
+//         //-------------o(n)----------------------------------
+//           vector<int> ans = {-1, -1}; // Initialize the result vector with default values.
 
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] == t) {
-                ans[0] = i; // Update the first occurrence.
-                break; // Exit the loop after finding the first occurrence.
-            }
+//         for (int i = 0; i < a.size(); i++) {
+//             if (a[i] == t) {
+//                 ans[0] = i; // Update the first occurrence.
+//                 break; // Exit the loop after finding the first occurrence.
+//             }
+//         }
+
+//         for (int j = a.size() - 1; j >= 0; j--) {
+//             if (a[j] == t) {
+//                 ans[1] = j; // Update the last occurrence.
+//                 break; // Exit the loop after finding the last occurrence.
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+ public:
+
+int bs_leftmost(vector<int>& nums, int k, int n){
+    int l = 0,  r=  n-1;
+
+    int leftmost = -1;
+
+    while(l <= r){
+        int mid = l + (r-l)/2;
+
+        if(nums[mid] == k){
+            leftmost = mid;
+            r = mid-1;
+        }else if(nums[mid] < k){
+            l = mid+1;
+        }else{
+            r = mid - 1;
         }
+    }
+    return leftmost;
+}
 
-        for (int j = a.size() - 1; j >= 0; j--) {
-            if (a[j] == t) {
-                ans[1] = j; // Update the last occurrence.
-                break; // Exit the loop after finding the last occurrence.
-            }
+int bs_rightmost(vector<int>& nums, int k, int n){
+    int l = 0,  r=  n-1;
+
+    int rightmost = -1;
+
+    while(l <= r){
+        int mid = l + (r-l)/2;
+
+        if(nums[mid] == k){
+            rightmost = mid;
+            l = mid+1;
+        }else if(nums[mid] < k){
+            l = mid+1;
+        }else{
+            r = mid - 1;
         }
+    }
+    return rightmost;
+}
 
-        return ans;
+    vector<int> searchRange(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int leftmost = bs_leftmost(nums, k, n);
+
+        int rightmost = bs_rightmost(nums, k, n);
+
+        return {leftmost, rightmost};
     }
 };
