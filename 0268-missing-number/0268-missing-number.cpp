6d5fff13edf5eda1105ee0dfb86bfd55d474a4ea
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-28 15:15:10 +0530

    Time: 21 ms (13.94%), Space: 21.1 MB (13.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        
-        int x = n*(n+1)/2;
-        
-        int sum = 0;
-        
-        for(int i=0; i<n; i++){
-            sum += nums[i];
+    
+
+    int solve(vector<int>& nums){
+         
+         sort(nums.begin(), nums.end());
+
+        int l = 0; int r = nums.size()-1;
+
+        while(l <= r){
+            int mid = l +(r-l)/2;
+
+            if(nums[mid] > mid ){
+                r = mid-1;
+            }else{
+                l  = mid + 1;
+            }
         }
+        return l;
+    }
+  
+
+    int missingNumber(vector<int>& nums) {
+        // int n = nums.size();
         
-        return x - sum;
+        // int x = n*(n+1)/2;
+        
+        // int sum = 0;
+        
+        // for(int i=0; i<n; i++){
+        //     sum += nums[i];
+        // }
+        
+        // return x - sum;
+
+//by binary search
+        return solve(nums);
     }
 };
