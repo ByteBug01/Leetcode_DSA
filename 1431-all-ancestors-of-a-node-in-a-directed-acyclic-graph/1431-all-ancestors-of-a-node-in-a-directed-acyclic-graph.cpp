4a7b94d462611dac60d04f5cadb4345da86d7acf
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-06-29 13:31:06 +0530

    Time: 197 ms (44.9%), Space: 74.5 MB (63.34%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,32 +1,82 @@
+// class Solution {
+// public:
+
+// void dfs(int ancestor,unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& ancestors) {
+//         for (int v : adj[currNode]) {
+//             if (ancestors[v].empty() || ancestors[v].back() != ancestor) {
+//                 ancestors[v].push_back(ancestor);
+//                 dfs(ancestor, adj, v, ancestors);
+//             }
+//         }
+// }
+
+
+//     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+//         unordered_map<int, vector<int>> adj;
+
+//         for(auto & edge: edges){
+//             int u = edge[0];
+//             int v  = edge[1];
+
+//             adj[u].push_back(v);
+//         }
+
+//       vector<vector<int>> ances(n);
+
+//         for(int i  = 0; i<n; i++){
+//             dfs(i, adj, i, ances); //first  i  = ancestor, 2nd i  =curr_node
+//         }
+
+//         return ances;
+//     }
+// };
+
+//-----------2nd approach by reversing the graph and easy dfs-------------------------------
+
 class Solution {
 public:
 
-void dfs(int ancestor,unordered_map<int, vector<int>>& adj, int currNode, vector<vector<int>>& ancestors) {
-        for (int v : adj[currNode]) {
-            if (ancestors[v].empty() || ancestors[v].back() != ancestor) {
-                ancestors[v].push_back(ancestor);
-                dfs(ancestor, adj, v, ancestors);
-            }
+void dfs(int u,  unordered_map<int, vector<int>> &adj, vector<bool>& visited)
+{
+    visited[u] = true;
+
+    for(int & v: adj[u]){
+        if(!visited[v]){
+            dfs(v, adj, visited);
         }
+    }
 }
 
 
-    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+
+ vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adj;
 
         for(auto & edge: edges){
             int u = edge[0];
             int v  = edge[1];
 
-            adj[u].push_back(v);
+            adj[v].push_back(u); // v ---> u, child to parent
         }
 
-vector<vector<int>> ances(n);
+vector<vector<int>> ans;
 
-        for(int i  = 0; i<n; i++){
-            dfs(i, adj, i, ances); //first  i  = ancestor, 2nd i  =curr_node
+for(int u = 0; u<n; u++){
+
+    vector<int> ancestors;
+    vector<bool> visited(n, false);
+    dfs(u, adj, visited);
+
+    for(int i = 0; i<n; i++){
+        if(i == u) continue; //as node cannot be ancestor of itself
+
+        if(visited[i] == true){
+            ancestors.push_back(i);
         }
-
-        return ances;
     }
+ ans.push_back(ancestors);
+}
+
+return ans;
+ }
 };
