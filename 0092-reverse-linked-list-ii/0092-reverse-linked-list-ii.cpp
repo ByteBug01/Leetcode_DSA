96a858632f87483f7c8e2be391c84561842f46d1
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-11 22:53:17 +0530

    Time: 5 ms (12.38%), Space: 10.6 MB (14.81%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -8,32 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseBetween(ListNode* head, int left, int right) {
+//         stack<int> s;
+//         ListNode* temp1 = head;
+//         ListNode* temp2 = head;
+//         int le = left;
+//         while(--left){  
+//             temp1 = temp1->next;
+//         }
+//         while(--right){  
+//             temp2 = temp2->next;
+//         }
+//         while(temp1 != temp2->next){  
+//             s.push(temp1->val);
+//             temp1 = temp1->next;
+//         }
+//         temp1 = head;
+//         while(--le){   
+//             temp1 = temp1->next;
+//         }
+//         while(s.size()){   
+//             temp1->val = s.top();
+//             temp1 = temp1->next;
+//             s.pop();
+//         }
+//         return head;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        stack<int> s;
-        ListNode* temp1 = head;
-        ListNode* temp2 = head;
-        int le = left;
-        while(--left){  
-            temp1 = temp1->next;
+        if(head == NULL || head->next == NULL){
+            return head;
         }
-        while(--right){  
-            temp2 = temp2->next;
+
+        ListNode* dummy = new ListNode(0);
+
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+
+        for(int i =  1; i<left; i++){
+            prev = prev->next;
         }
-        while(temp1 != temp2->next){  
-            s.push(temp1->val);
-            temp1 = temp1->next;
+
+        ListNode* curr = prev->next;
+
+        for(int i = 1; i<= right-left; i++){
+            ListNode* temp =  prev->next; //temp = b
+
+            prev->next = curr->next; // a->next = c
+            curr->next = curr->next->next; // b->next = c->next //d
+
+            prev->next->next  = temp; // c->next  = b
         }
-        temp1 = head;
-        while(--le){   
-            temp1 = temp1->next;
-        }
-        while(s.size()){   
-            temp1->val = s.top();
-            temp1 = temp1->next;
-            s.pop();
-        }
-        return head;
+
+        return dummy->next;
     }
 };
