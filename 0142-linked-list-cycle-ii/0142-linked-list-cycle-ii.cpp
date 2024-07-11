d2f44e7fef72c9d2f6b35820aed430ad0933b7a9
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-11 23:16:16 +0530

    Time: 10 ms (28.23%), Space: 10.4 MB (18.31%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,25 +6,57 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// class Solution {
+// public:
+
+// // Bruteforce------------------------------
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_map<ListNode* , int> mp;
+        
+//         ListNode* temp = head;
+
+//         while(temp != NULL){
+
+//             if(mp[temp] != 0){ // check if temp has been encountered again
+//                 return temp; //  A loop is detected hence return temp
+//             }
+
+//             mp[temp] = 1; 
+//             temp = temp->next;
+//         }
+
+//         return NULL;
+//     }
+// };
+
+
+//-------------optimal (with slow , fast pointer)--------------------
 class Solution {
 public:
 
-// Bruteforce------------------------------
-    ListNode *detectCycle(ListNode *head) {
-        unordered_map<ListNode* , int> mp;
-        
-        ListNode* temp = head;
+  ListNode *detectCycle(ListNode *head) {
+      ListNode* slow = head;
+      ListNode* fast = head;
 
-        while(temp != NULL){
+      while(fast != NULL && fast->next != NULL){
+          slow = slow->next;
+          fast = fast->next->next;
 
-            if(mp[temp] != 0){ // check if temp has been encountered again
-                return temp; //  A loop is detected hence return temp
-            }
+          if(fast == slow){ //cycle detected
+                  slow = head; //  // Reset the slow point to the head 
 
-            mp[temp] = 1; 
-            temp = temp->next;
-        }
+                  while(slow != fast){  // When slow and fast meet again it's the first node of the loop
 
-        return NULL;
-    }
+                    slow = slow->next;
+                    fast = fast->next;
+                  }
+
+                   return slow;
+          }
+      }
+
+return NULL;
+     
+  }
 };
