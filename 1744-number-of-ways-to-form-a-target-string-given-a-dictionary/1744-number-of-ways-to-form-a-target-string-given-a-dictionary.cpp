8ff5e45badac716d10bc807c442805a4d4292f67
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-19 23:04:33 +0530

    Time: 188 ms (44.06%), Space: 84.2 MB (17.24%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
+//---------------memoization-----------------------------------
+ int n, k;
+ const int mod = 1000000007;
+// int t[1001][1001];
 
-int n, k;
-const int mod = 1000000007;
-int t[1001][1001];
+// int solve(int i , int j,   vector<vector<long long>>& frq, string & target){
+//     if( i == n) return 1;
 
-int solve(int i , int j,   vector<vector<long long>>& frq, string & target){
-    if( i == n) return 1;
+//     if(j == k) return 0;
 
-    if(j == k) return 0;
+//     if(t[i][j] != -1) return t[i][j];
 
-    if(t[i][j] != -1) return t[i][j];
+//     int not_take = solve(i, j+1, frq, target)%mod;
+//     int take = (frq[target[i]-'a'][j] * solve(i+1, j+1, frq, target))% mod;
 
-    int not_take = solve(i, j+1, frq, target)%mod;
-    int take = (frq[target[i]-'a'][j] * solve(i+1, j+1, frq, target))% mod;
-
-    return t[i][j] = (take + not_take) % mod;
-}
+//     return t[i][j] = (take + not_take) % mod;
+// }
 
     int numWays(vector<string>& words, string target) {
           n = target.length();
@@ -32,8 +32,26 @@ int solve(int i , int j,   vector<vector<long long>>& frq, string & target){
             }
         }
 
-        memset(t, -1, sizeof(t));
+        // memset(t, -1, sizeof(t));
 
-        return solve(0, 0, frq, target);
+        // return solve(0, 0, frq, target);
+
+        vector<vector<long long>> t(n+1, vector<long long>(k+1));
+
+        t[0][0] = 1;
+
+        for(int i = 0; i<=n; i++){
+            for(int j = 0; j<=k; j++){
+                //not_take
+                if(j<k)
+                t[i][j+1] = (t[i][j+1] + t[i][j]) % mod;
+
+                //take
+                if(i<n && j<k)
+                t[i+1][j+1] = (t[i+1][j+1] + t[i][j] * frq[target[i] - 'a'][j])%mod;
+            }
+        }
+
+        return t[n][k];
     }
 };
