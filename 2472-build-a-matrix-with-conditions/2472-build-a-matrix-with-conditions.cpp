0ac472b09db0c1697c83625f256024b559be718d
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-22 17:34:41 +0530

    Time: 90 ms (86.11%), Space: 61.1 MB (73.89%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,48 +1,92 @@
 class Solution {
 public:
 
-    void dfs(int u, unordered_map<int, vector<int>>& adj, vector<int>& visited, stack<int>& st, bool& cycle) {
-        visited[u] = 1; // Mark the node as visiting
+//-----------------topo Sort by dfs---------------------------------------------
+    // void dfs(int u, unordered_map<int, vector<int>>& adj, vector<int>& visited, stack<int>& st, bool& cycle) {
+    //     visited[u] = 1; // Mark the node as visiting
 
-        for (auto& v : adj[u]) {
-            if (visited[v] == 0) {
-                dfs(v, adj, visited, st, cycle);
-            } else if (visited[v] == 1) {
-                cycle = true; // Cycle detected
-                return;
+    //     for (auto& v : adj[u]) {
+    //         if (visited[v] == 0) {
+    //             dfs(v, adj, visited, st, cycle);
+    //         } else if (visited[v] == 1) {
+    //             cycle = true; // Cycle detected
+    //             return;
+    //         }
+    //     }
+
+    //     visited[u] = 2; // Mark the node as visited
+    //     st.push(u); 
+    // }
+
+    // vector<int> topo(int k, vector<vector<int>>& condition) {
+    //     vector<int> order;
+    //     unordered_map<int, vector<int>> adj;
+
+    //     for (auto& it : condition) {
+    //         int u = it[0];
+    //         int v = it[1];
+    //         adj[u].push_back(v);
+    //     }
+
+    //     vector<int> visited(k + 1, 0);
+    //     stack<int> st;
+    //     bool cycle = false;
+
+    //     for (int i = 1; i <= k; i++) {
+    //         if (visited[i] == 0) {
+    //             dfs(i, adj, visited, st, cycle);
+    //             if (cycle) {
+    //                 return {};
+    //             }
+    //         }
+    //     }
+
+    //     while (!st.empty()) {
+    //         order.push_back(st.top());
+    //         st.pop();
+    //     }
+
+    //     return order;
+    // }
+
+    //-----------------------topo SOrt by BFS--------------------------------------------
+   vector<int> topo(int k, vector<vector<int>>& conditions) {
+        unordered_map<int, vector<int>> adj(k + 1);
+        vector<int> indegree(k + 1, 0);
+
+        for (vector<int>& edge : conditions) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+        for (int i = 1; i <= k; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        visited[u] = 2; // Mark the node as visited
-        st.push(u); 
-    }
-
-    vector<int> topo(int k, vector<vector<int>>& condition) {
         vector<int> order;
-        unordered_map<int, vector<int>> adj;
+        int cnt = 0;
 
-        for (auto& it : condition) {
-            int u = it[0];
-            int v = it[1];
-            adj[u].push_back(v);
-        }
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            order.push_back(node);
+            cnt++;
 
-        vector<int> visited(k + 1, 0);
-        stack<int> st;
-        bool cycle = false;
-
-        for (int i = 1; i <= k; i++) {
-            if (visited[i] == 0) {
-                dfs(i, adj, visited, st, cycle);
-                if (cycle) {
-                    return {};
+            for (auto& v : adj[node]) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.push(v);
                 }
             }
         }
 
-        while (!st.empty()) {
-            order.push_back(st.top());
-            st.pop();
+        if (cnt != k) {
+            return {}; // cycle detected, not all nodes were visited
         }
 
         return order;
