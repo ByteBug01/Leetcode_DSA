d994635be6b405a8da43b882e741cb61a020804a
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-24 00:18:22 +0530

    Time: 12 ms (63.12%), Space: 12 MB (40.6%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,24 +1,48 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//       int max=0;
+//         int start = 0;
+//         int end = 0;
+//         set<int> Set;
+        
+//         while(start<s.size()){
+//             auto it = Set.find(s[start]);
+            
+//             if(it==Set.end()){
+//                 if(start-end+1 > max)
+//                     max = start-end+1;  // Update the maximum length if a longer substring is found
+//                 Set.insert(s[start]);
+//                 start++; // Expand the window to the right
+//             }else{
+//                 Set.erase(s[end]); // Remove the character at the beginning of the window from the set
+//                 end++;  // Slide the window to the right
+//             }
+//         }return max;
+//     }
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.length();
-        int maxLength = 0;
-        unordered_set<char> charSet;
+
+      int n = s.length();
+        unordered_map<char, int> mp;
+
         int left = 0;
-        
-        for (int right = 0; right < n; right++) {
-            if (charSet.count(s[right]) == 0) {
-                charSet.insert(s[right]);
-                maxLength = max(maxLength, right - left + 1);
-            } else {
-                while (charSet.count(s[right])) {
-                    charSet.erase(s[left]);
-                    left++;
-                }
-                charSet.insert(s[right]);
+        int right = 0;
+        int len = 0;
+
+        while (right < n) {
+            if (mp.find(s[right]) != mp.end()) {
+                left = max(mp[s[right]] + 1, left);
             }
+
+            mp[s[right]] = right;
+            len = max(len, right - left + 1);
+            right++;
         }
-        
-        return maxLength;
+
+        return len;
     }
 };
