9f07448b2b43b0291f5a64e3538d593580e7635e
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-24 13:44:14 +0530

    Time: 126 ms (88.03%), Space: 67.1 MB (73.93%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -1,19 +1,52 @@
+// class Solution {
+// public:
+
+// // sorting-------------O(nlogn)---------------
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         vector<tuple<int, int, int>> p;
+
+//         for (auto& it : points) {
+//             int x = it[0] * it[0] + it[1] * it[1];
+//             p.push_back(make_tuple(x, it[0], it[1]));
+//         }
+
+//         sort(p.begin(), p.end());
+
+//         vector<vector<int>> ans;
+
+//         for (int i = 0; i < k; i++) {
+//             ans.push_back({ get<1>(p[i]), get<2>(p[i]) });
+//         }
+
+//         return ans;
+//     }
+// };
+
+//-----------------using max heap-----------------------------------
+
 class Solution {
 public:
+
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        vector<tuple<int, int, int>> p;
+        priority_queue<pair<int, pair<int, int>>> pq;
 
-        for (auto& it : points) {
-            int x = it[0] * it[0] + it[1] * it[1];
-            p.push_back(make_tuple(x, it[0], it[1]));
+        for(auto & it: points){
+            int x = it[0]*it[0] + it[1]*it[1];
+
+            if(pq.size() < k){
+                pq.push({x, {it[0], it[1]}});
+            }else{
+                if(x < pq.top().first){
+                    pq.pop();
+                    pq.push({x, {it[0], it[1]}});
+                }
+            }
         }
-
-        sort(p.begin(), p.end());
-
         vector<vector<int>> ans;
 
-        for (int i = 0; i < k; i++) {
-            ans.push_back({ get<1>(p[i]), get<2>(p[i]) });
+        while(!pq.empty()){
+            ans.push_back({pq.top().second.first, pq.top().second.second});
+            pq.pop();
         }
 
         return ans;
