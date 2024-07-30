9a22bb0adb17f48eefb1720fb6c94fe1bfc66ad0
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-07-30 22:41:34 +0530

    Time: 114 ms (25.84%), Space: 53.2 MB (18.5%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -54,22 +54,63 @@
 //     }
 // };
 
-//approach - 1:
-class Solution {
-public:
-    int minimumDeletions(string s) {
+//approach - 1:--------------------------------------
+// class Solution {
+// public:
+//     int minimumDeletions(string s) {
             
-            stack<char> st;
-            int cnt = 0;
-            for(auto & ch: s){
-                if(!st.empty() && st.top() == 'b' && ch == 'a'){ //cnt 'ba'
-                   st.pop();
-                    cnt++;
-                }else{
-                    st.push(ch);
-                }
-            }
+//             stack<char> st;
+//             int cnt = 0;
+//             for(auto & ch: s){
+//                 if(!st.empty() && st.top() == 'b' && ch == 'a'){ //cnt 'ba'
+//                    st.pop();
+//                     cnt++;
+//                 }else{
+//                     st.push(ch);
+//                 }
+//             }
 
-            return cnt;
+//             return cnt;
+//     }
+// };
+
+
+//approach-2:--------------------------------------------------
+
+class Solution {
+ public:
+    int minimumDeletions(string s) {
+
+        int n = s.length();
+
+        vector<int> left_b_cnt(n, 0);
+        vector<int> right_a_cnt(n, 0);
+
+int cnt_b = 0;
+        for(int i= 0; i<n; i++){
+            left_b_cnt[i] = cnt_b;
+
+            if(s[i] == 'b'){
+                cnt_b++;
+            }
+        }
+
+        int cnt_a = 0;
+
+        for(int i = n-1; i>=0; i--){
+            right_a_cnt[i] = cnt_a;
+
+            if(s[i] == 'a'){
+                cnt_a++;
+            }
+        }
+
+int minCnt = INT_MAX;
+        for(int i = 0; i<n; i++){
+             minCnt = min(minCnt, left_b_cnt[i]+right_a_cnt[i]);
+        }
+
+
+        return minCnt;
     }
 };
