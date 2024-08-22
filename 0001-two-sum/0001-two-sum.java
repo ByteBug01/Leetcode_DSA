2e82e75dc222e08d4ad3141f961fa0d9a78b2dd1
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-08-22 21:21:47 +0530

    Time: 9 ms (54.15%), Space: 44.3 MB (97.05%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+          int n = nums.length;
+
+        Integer[] idxArr= new Integer[n];
+        
+        for (int i = 0; i < n; i++) {
+            idxArr[i] = i;
+        }
+        
+        Arrays.sort(idxArr, (a, b) -> Integer.compare(nums[a], nums[b])); //sort doesn't work with primitive types like int[] directly. Instead, it works with object types.
+        int i = 0, j = n - 1;
+        int[] ans = new int[2];
+        
+        while (i < j) {
+            int sum = nums[idxArr[i]] + nums[idxArr[j]];
+            if (sum == target) {
+                ans[0] = idxArr[i];
+                ans[1] = idxArr[j];
+                return ans;
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return ans;
+    }
+}
