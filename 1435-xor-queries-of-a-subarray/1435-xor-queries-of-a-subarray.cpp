a6cd86366443cad07b54f22f7c114f5ec86d89bb
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-09-13 10:04:36 +0530

    Time: 56 ms (74.57%), Space: 42.1 MB (42.84%) - LeetHub

--- 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        vector<int> ans;
+        // vector<int> ans;
         
-        for(auto & it: queries){
+        // for(auto & it: queries){
+        //     int a = it[0];
+        //     int b = it[1];
+        //     int tmp = 0;
+        //     for(int i = a; i <= b; i++){
+        //         tmp ^= arr[i];  
+        //     }
+        //     ans.push_back(tmp);
+        // }
+        // return ans;
+
+        vector<int> ans;
+        int n = arr.size();
+        vector<int> xorPrefix(n+1, 0);
+
+        for(int i=0; i<n; i++){
+           xorPrefix[i+1] = xorPrefix[i] ^ arr[i];
+        }
+
+        for(auto& it: queries){
             int a = it[0];
             int b = it[1];
-            int tmp = 0;
-            for(int i = a; i <= b; i++){
-                tmp ^= arr[i];  
-            }
-            ans.push_back(tmp);
+            
+            ans.push_back(xorPrefix[b+1] ^ xorPrefix[a]);
         }
+
         return ans;
     }
 };
