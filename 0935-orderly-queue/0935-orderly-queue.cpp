127987047ae1379773306243353d0a9a6cd0c186
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-09-23 23:46:18 +0530

    Time: 5 ms (24.76%), Space: 12.9 MB (50.89%) - LeetHub

--- 0935-orderly-queue/0935-orderly-queue.cpp
+++ 0935-orderly-queue/0935-orderly-queue.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+//very easy problm
+//just do, sort if k > 1
+//else if k == 1, apply bruteforce 
+
     string orderlyQueue(string s, int k) {
-        int n = s.length();
         if(k > 1){
             sort(s.begin(), s.end());
             return s;
         }
 
         string ans = s;
-        for(int i =1; i<= n-1; i++){
-            string temp = s.substr(i) + s.substr(0, i);
-            ans = min(ans, temp);
+        int n = s.length();
+        for(int i = 0; i<=n-1; i++){
+            string tmp = s.substr(i) + s.substr(0, i);
+            ans = min(ans, tmp);
         }
 
         return ans;
