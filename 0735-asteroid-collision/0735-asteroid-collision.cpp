16eb361a85af3958a02ff4e17db3114d1ebce2dd
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-09-29 00:43:43 +0530

    Time: 14 ms (18.14%), Space: 20.7 MB (65.99%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,37 +1,74 @@
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& asteroids) {
+//         int n = asteroids.size();
+//         stack<int> st;
+      
+//       for(auto& a: asteroids){
+//            while(!st.empty() && (a < 0 && st.top() > 0)){
+//                int sum  = a+st.top();
+//                if(sum<0){ //a > st.top() & oppo direction
+//                      st.pop();
+//                }else if(sum > 0){
+//                 a = 0;
+//                }else{
+//                 //equal strength
+//                  st.pop();
+//                  a=0;
+//                }
+//            }
+
+//            if(a != 0){
+//             st.push(a);
+//            }
+//       }
+
+// int i = st.size();
+// vector<int> ans(i);
+
+// while(!st.empty()){
+//      ans[i-1] = st.top();
+//      st.pop();
+//      i--;
+// }
+
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
-        stack<int> st;
-      
-      for(auto& a: asteroids){
-           while(!st.empty() && (a < 0 && st.top() > 0)){
-               int sum  = a+st.top();
-               if(sum<0){ //a > st.top()
-                     st.pop();
-               }else if(sum > 0){
-                a = 0;
-               }else{
-                //equal strength
-                 st.pop();
-                 a=0;
-               }
-           }
+     int n = asteroids.size();
+     stack<int> st;
 
-           if(a != 0){
-            st.push(a);
-           }
-      }
+     for(auto & ast: asteroids){
+        bool explode = false;
+        while(!st.empty() && st.top() >0 && ast < 0){
+            if(abs(st.top()) < abs(ast)){
+                st.pop();
+            }else if(abs(st.top()) == abs(ast)){
+                st.pop();
+                explode = true;
+                break;
+            }else{
+                explode = true;
+                break;
+            }
 
-int i = st.size();
-vector<int> ans(i);
+        }
+        if(!explode) st.push(ast);
+     }
 
-while(!st.empty()){
-     ans[i-1] = st.top();
-     st.pop();
-     i--;
-}
 
-        return ans;
+     vector<int> ans(st.size());
+
+     for(int i = st.size()-1; i>=0; i--){
+        ans[i] = st.top();
+        st.pop();
+     }
+
+     return ans;
     }
 };
