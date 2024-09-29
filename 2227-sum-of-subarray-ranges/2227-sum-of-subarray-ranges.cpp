025c46e6c880779637c3d79d0e6e1692a362ca3a
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-09-30 00:37:51 +0530

    Time: 6 ms (97.43%), Space: 14.4 MB (55.95%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,19 +1,85 @@
+// class Solution {
+// public:
+//     long long subArrayRanges(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         long long ans = 0;
+
+//         for(int i =0; i<n; i++){
+//             int maxi = nums[i], mini = nums[i];
+//           for(int j = i; j<n; j++){
+//               maxi = max(maxi, nums[j]);
+//               mini = min(mini, nums[j]);
+
+//               ans += (maxi - mini);
+//           }
+//         }
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+
         int n = nums.size();
-        
-        long long ans = 0;
+        long long totsum = 0;
+     
+     vector<int> nextgreater(n, n), prevgreater(n, -1);
+     stack<int> st;
 
-        for(int i =0; i<n; i++){
-            int maxi = nums[i], mini = nums[i];
-          for(int j = i; j<n; j++){
-              maxi = max(maxi, nums[j]);
-              mini = min(mini, nums[j]);
-
-              ans += (maxi - mini);
-          }
+     for(int i=0; i<n;i++){
+        while(!st.empty() && nums[st.top()] < nums[i]){
+            nextgreater[st.top()] = i;
+            st.pop();
         }
-        return ans;
+        st.push(i);
+     }
+
+
+     while(!st.empty()) st.pop(); //reuse
+
+     for(int i=n-1; i>=0;i--){
+        while(!st.empty() && nums[st.top()] <= nums[i]){
+            prevgreater[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+     }
+
+     for(int i = 0; i<n; i++){
+        totsum += (long long)(i - prevgreater[i])* (nextgreater[i] - i) * nums[i];
+     }
+     
+     vector<int> nextsmaller(n, n), prevsmaller(n, -1);
+  //   stack<int> st;
+     while(!st.empty()) st.pop(); //reuse
+
+     for(int i=0; i<n;i++){
+        while(!st.empty() && nums[st.top()] > nums[i]){
+            nextsmaller[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+     }
+
+
+     while(!st.empty()) st.pop(); //reuse
+
+     for(int i=n-1; i>=0;i--){
+        while(!st.empty() && nums[st.top()] >= nums[i]){
+            prevsmaller[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+     }
+
+     for(int i = 0; i<n; i++){
+        totsum -= (long long)(i - prevsmaller[i])* (nextsmaller[i] - i) * nums[i];
+     }
+
+
+return totsum;
     }
 };
