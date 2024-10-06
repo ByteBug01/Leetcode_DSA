f248c8d15b810717b65871cfe0a7033184e7d5f5
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-10-06 12:29:16 +0530

    Time: 0 ms (100%), Space: 9.7 MB (64.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,63 +1,97 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//     //     int n = s.size();
+
+//     //     stack<char> st;
+
+//     //     for(auto ch: s){
+//     //         st.push(ch);
+//     //     }
+
+//     //    string ans = "";
+
+//     //     while(!st.empty()){
+//     //        string temp = "";
+//     //        temp += st.top();
+//     //        st.pop();
+//     //        if(st.top() == ' '){
+//     //         reverse(temp.begin(), temp.end());
+//     //         ans = temp + " ";
+//     //         temp = "";
+//     //        } 
+//     //     }
+
+//     //     return ans;
+
+//         //instead of pushing a character into stack push whole one word
+
+//         int n = s.size();
+//         stack<string> st;
+//         string temp = "";
+        
+//         for(int i = 0; i < n; ++i) {
+//             if(s[i] == ' ') {
+//                 if(!temp.empty()) {
+//                     st.push(temp);
+//                     temp = "";
+//                 }
+//             } else {
+//                 temp += s[i];
+//             }
+//         }
+        
+        
+//         if(!temp.empty()) {
+//             st.push(temp);
+//         }
+
+//         string ans = "";
+        
+    
+//         while(!st.empty()) {
+//             ans += st.top();
+//             st.pop();
+//             if(!st.empty()) {
+//                 ans += " ";
+//             }
+//         }
+        
+//         return ans;
+        
+
+//     }
+// };
+
+
 class Solution {
 public:
     string reverseWords(string s) {
-    //     int n = s.size();
-
-    //     stack<char> st;
-
-    //     for(auto ch: s){
-    //         st.push(ch);
-    //     }
-
-    //    string ans = "";
-
-    //     while(!st.empty()){
-    //        string temp = "";
-    //        temp += st.top();
-    //        st.pop();
-    //        if(st.top() == ' '){
-    //         reverse(temp.begin(), temp.end());
-    //         ans = temp + " ";
-    //         temp = "";
-    //        } 
-    //     }
-
-    //     return ans;
-
-        //instead of pushing a character into stack push whole one word
-
-        int n = s.size();
         stack<string> st;
-        string temp = "";
-        
-        for(int i = 0; i < n; ++i) {
-            if(s[i] == ' ') {
-                if(!temp.empty()) {
-                    st.push(temp);
-                    temp = "";
-                }
-            } else {
-                temp += s[i];
-            }
+
+        string word = "";
+        for(char &ch: s){
+           if(ch == ' '){
+            if(!word.empty())
+             { st.push(word);
+              word = "";
+              }
+           }
+           else word += ch;
         }
-        
-        
-        if(!temp.empty()) {
-            st.push(temp);
-        }
+
+        if(!word.empty()) st.push(word);
 
         string ans = "";
-        
-    
-        while(!st.empty()) {
+        while(!st.empty()){
             ans += st.top();
             st.pop();
-            if(!st.empty()) {
+
+            if(!st.empty()){
                 ans += " ";
             }
-        }
-        
-        return ans;
 
-    }
-};
+        }
+
+        return ans;
+    }};
