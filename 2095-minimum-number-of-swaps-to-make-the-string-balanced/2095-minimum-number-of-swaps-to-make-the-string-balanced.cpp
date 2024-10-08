4424628a267bfae4e6e7fd267f44c1457efa83d5
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-10-08 13:26:47 +0530

    Time: 94 ms (88.68%), Space: 32.7 MB (54.72%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,18 +1,41 @@
+//approach - 1
+// class Solution {
+// public:
+//     int minSwaps(string s) {
+//         int n = s.length();
+//         stack<char> st;
+
+//         for(char ch: s){
+//             if(ch == '['){
+//                 st.push(ch);
+//             }
+//            else if(!st.empty() && ch == ']' && st.top() == '['){
+//                 st.pop();
+//             }
+//         }
+
+//         return (st.size() + 1)/2;
+//     }
+// };
+
+//approach - 2
 class Solution {
 public:
     int minSwaps(string s) {
-        int n = s.length();
-        stack<char> st;
+        int open = 0, close = 0;
 
-        for(char ch: s){
-            if(ch == '['){
-                st.push(ch);
-            }
-           else if(!st.empty() && ch == ']' && st.top() == '['){
-                st.pop();
+        for (char ch : s) {
+            if (ch == '[') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    close++;
+                }
             }
         }
 
-        return (st.size() + 1)/2;
+        return (close + 1) / 2;
     }
 };
