f34e88053bc11b71ae65dbe872c6abed1c6200c6
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-11-04 02:15:27 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.5%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,15 +1,33 @@
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         //All rotations of A are contained in A+A. Thus, we can simply 
+//         //check whether B is a substring of A+A.
+
+//        if (s.length() != goal.length()) {
+//             return false;
+//         }
+
+//         string ss = s + s;
+//         //check if goal is substring of concatenated string or not
+//         return ss.find(goal) != string::npos;
+//     }
+// };
+
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        //All rotations of A are contained in A+A. Thus, we can simply 
-        //check whether B is a substring of A+A.
 
-       if (s.length() != goal.length()) {
+            if (s.length() != goal.length()) return false;
+
+            int n=s.length();
+
+            for(int i=0; i<n; i++){
+                rotate(s.begin(), s.begin() + 1, s.end());
+
+                if(s == goal) return true;
+            }
+
             return false;
-        }
-
-        string ss = s + s;
-        //check if goal is substring of concatenated string or not
-        return ss.find(goal) != string::npos;
     }
 };
