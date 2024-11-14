45c89b11b700e50b3c3e57716a6d3b01603c9679
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2024-11-14 23:00:05 +0530

    Time: 3 ms (91.76%), Space: 22.8 MB (88.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-        
-       sort(arr.begin(),arr.end() );
-        
-        int n = arr.size();
-        
-            vector<vector<int>> ans;
-        for(int i=0; i<n; i++){
-            if(ans.empty()){
-                ans.push_back(arr[i]);
-            }
-            else{
-                vector<int>& v = ans.back();
-                int y= v[1]; // end of previous interval
-                //comapare 'y' with i's start time
-                
-                //overlapping
-                if(arr[i][0]<=y){
-                    v[1]=max(y, arr[i][1]);
-                }
-                //not overlapping
-                else
-                {
-                    ans.push_back(arr[i]);
-                }
+      int n = arr.size();
+      sort(arr.begin(), arr.end());
+
+      vector<vector<int>> ans;
+
+      for(int i=0; i<n; i++){
+        int start = arr[i][0];
+        int end = arr[i][1];
+
+        if(!ans.empty() && end <= ans.back()[1]){  //yha pe jo curr ele ka end hai wo ans k last ele k end se chotta hoga 
+                                                   //toh wo already covered hai ans ke interval me 
+            continue;
+        }
+
+        for(int j =i+1; j<n; j++){
+            if(arr[j][0] <= end){   //next ele ka start <= curr k end then , update end jo max hoga
+                end  = max(arr[j][1], end);
+            }else{        //otherwise new interval
+                break;
             }
         }
-        return ans;
+
+        ans.push_back({start, end});
+      
+      }
+
+      return ans;
     }
-}; 
+};
