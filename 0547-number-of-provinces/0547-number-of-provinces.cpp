657bf9cb3d7b6f68d209e922cdf2a4e3ec4d304f
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2025-04-18 00:26:42 +0530

    Time: 4 ms (30.79%), Space: 21 MB (22.78%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,71 +1,140 @@
 // class Solution {
-//     private:
-//     void dfs(int node , vector<int> adjLs[], int vis){
-//         vis[node] = 1;
-//         for(auto it: adjLs[node]){
-//             if(!vis[it]){
-//                 dfs(it, adjLs, vis);
-//             }
-//         }
-//     }
-    
 // public:
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         int n = isConnected.size();
-        
-//         vector<int> adjLs[n];
-        
-//         for(itn i=0; i<n; i++){
-//             for(int j=0; j<n; j++){
-//                 if( isConnected[i][j] == 1 && i!=j ){
-//                      adjLs[i].push_back(j);
-//                      adjLs[j].push_back(i);
-//                 }
-//             }
-//         }
-        
-//         int vis[n]={0};
-//         int cnt = 0;
-//         for(int i=0; i<n; i++){
-//             if(!vis[i]){
-//                 cnt++;
-//                 dfs(i, adjLs, vis);
-//             }
-//         }
-//         return cnt;
+
+// void dfs( unordered_map<int, vector<int>> adj, int u, vector<bool>& visited){
+// visited[u] = true;
+
+// for(auto & v : adj[u]){
+//     if(!visited[v]){
+//         dfs(adj, v, visited);
 //     }
+// }
+
+// }
+
+
+// int findCircleNum(vector<vector<int>>& isConnected) {
+
+//     int n = isConnected.size();
+
+//    unordered_map<int, vector<int>> adj;
+
+//    for(int i = 0; i<n; i++){
+//     for(int j = 0; j<n; j++){
+//         if(isConnected[i][j] == 1){
+//             adj[i].push_back(j);
+//             adj[j].push_back(i);
+//         }
+//     }
+//    }
+
+//    vector<bool> visited(n, false);
+
+//    int cnt  = 0;
+
+//    for(int i =0; i<n; i++){
+//     for(auto & v: adj[i]){
+//         if(!visited[v] ){
+//             dfs(adj, i, visited);
+//             cnt++;
+//         }
+//     }
+//    }
+
+// return cnt;
+
+// }
 // };
+
+//--------------------------------------------------------------------------
+//bfs
+// class Solution {
+// public:
+// int n;
+// void bfs(vector<vector<int>>& isConnected, int u , vector<bool> & visited){
+// queue<int> q;
+// q.push(u);
+
+// visited[u] = true;
+
+// while(!q.empty()){
+//  int curr = q.front();
+//  q.pop();
+
+//  for(int v = 0; v<n; v++){
+//      if(!visited[v] && isConnected[u][v] == 1){
+//         bfs(isConnected, v, visited);
+
+//      }
+//  }
+
+// }
+
+// }
+
+
+// int findCircleNum(vector<vector<int>>& isConnected) {
+//     n = isConnected.size();
+
+//    vector<bool> visited(n, false);
+
+//    int cnt  = 0;
+
+//    for(int  i = 0; i<n ;i++){
+//     if(!visited[i]){
+//         bfs(isConnected, i, visited);
+//         cnt++;
+//     }
+//    }
+
+// return cnt;
+// }
+// };
+
+//--------------------04/18------
 
 class Solution {
 public:
-    void dfs(int j,vector<bool> &vis,vector<int> adj[]){
-        vis[j]=true;
-        for(auto x:adj[j]){
-            if(!vis[x]){
-                dfs(x,vis,adj);
-            }
+
+void dfs(int i, vector<bool>& visited, unordered_map<int, vector<int>>& adj){
+    visited[i] = true;
+
+    for(auto & it: adj[i]){
+        if(!visited[it]){
+            dfs(it, visited, adj);
         }
     }
-    int findCircleNum(vector<vector<int>>& v) {
-        int n=(int)v.size();
-        const int N=444;
-        vector<int> adj[N];
-        vector<bool> vis(N);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(v[i][j]==1 and i!=j){
-                    adj[i].push_back(j);
-                }
-            }
+}
+
+int findCircleNum(vector<vector<int>>& isConnected) {
+      int n = isConnected.size();
+
+      unordered_map<int, vector<int>> adj;
+
+      for(int i = 0; i<n; i++){
+        for(int j =0; j<n; j++){
+              if(isConnected[i][j] == 1){
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+              }
         }
-        vis.assign(n+4,false);
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,vis,adj);
-                cnt++;
+      }
+
+        vector<bool>visited(n, false);
+        int provinceCnt =  0;
+
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(i, visited, adj);
+                provinceCnt++;
             }
+           
         }
-        return cnt;
-    }
+         return provinceCnt;
+      
+
+     
+}
 };
+
+
