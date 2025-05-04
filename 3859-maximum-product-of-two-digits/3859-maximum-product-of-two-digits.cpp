26a3f9c65bdd00e29981ad51c83fab91166086b0
VINITA SAHU <100153428+ByteBug01@users.noreply.github.com>
2025-05-04 12:12:02 +0530

    Time: 0 ms (100%), Space: 8.4 MB (100%) - LeetHub

--- 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        string s = to_string(n);
+        // string s = to_string(n);
 
-        sort(s.begin(), s.end());
+        // sort(s.begin(), s.end());
 
-        int nn = s.size();
+        // int nn = s.size();
 
-        int ans = (s[nn-1] - '0') * (s[nn-2] - '0');
+        // int ans = (s[nn-1] - '0') * (s[nn-2] - '0');
 
-        return ans;
+        // return ans;
 
+
+        // without sorting-----------------
+
+        int max1st = INT_MIN;
+        int max2nd;
+
+        while(n > 0){
+            int tmp = n%10;
+
+            if(tmp > max1st){
+                max2nd =  max1st;
+                max1st = tmp;
+            }else if(tmp > max2nd){
+                max2nd = tmp;
+            }
+            n/=10;
+        }
+       return max1st * max2nd;
     }
 };
